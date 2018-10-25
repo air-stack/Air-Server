0a2d079d57748267fe6fcb19068b56b171e73b23
kevinten10 <596823919@qq.com>
2018-10-25 15:39:25 +0800

    init

---
+++ src/main/java/com/ten/air/AirApplication.java
@@ -0,0 +1,12 @@
+package com.ten.air;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirApplication.class, args);
+    }
+}
